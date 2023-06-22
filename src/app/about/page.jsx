57c4f9a77c2650cfce6a15e9a07f8ce6d6a8801e
Thanat <bztzn512@gmail.com>
2023-06-22 10:12:00 -0400

    fix-wdith

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,21 +2,16 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="flex h-screen w-screen flex-col">
+    <div className=" h-screen w-screen">
       <div className="about-img flex h-[85vh] w-full items-center justify-center overflow-hidden bg-gray-300 bg-cover ">
         <h1 className="text-center text-6xl font-bold text-white">
           about fin studio
         </h1>
       </div>
-      <div className="container w-full justify-center">
-        <div className="relative items-center justify-center bg-slate-400 p-6 text-white">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
-            tempora pariatur voluptatem sed a praesentium modi repudiandae quam
-            magnam odio ad, necessitatibus et labore impedit tenetur! In, sit
-            illum
-          </p>
-        </div>
+
+      <div className="wrap container h-[500px] w-screen">
+        <div className="h-full w-full bg-green-300"></div>
+        <div className="h-full w-full bg-red-300"></div>
       </div>
     </div>
   );
