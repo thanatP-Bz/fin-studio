d4b7fc7744f8e2c2d94040e389bfc7c5c30f1dc0
Thanat <bztzn512@gmail.com>
2023-06-22 10:41:26 -0400

    .

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -5,7 +5,10 @@ const Hero = () => {
     <div>
       <div className="home-img relative flex h-screen w-full items-center justify-center overflow-hidden bg-gray-300 bg-cover">
         <div className=" container h-4/5 flex-col justify-end gap-y-2 p-10 text-white">
-          <h1 className="text-[5rem] font-bold "> Beyond Than Design</h1>
+          <h1 className="text-[5rem] font-bold uppercase">
+            {" "}
+            Beyond Than Design
+          </h1>
           <p className="flex w-full max-w-[1000px] justify-end p-2 text-2xl">
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa
             laudantium possimus quos provident similique nobis neque consectetur
