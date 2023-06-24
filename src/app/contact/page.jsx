1b00cc50954405ecbf0cae0d9a92ea9c1f3fd45f
Thanat <bztzn512@gmail.com>
2023-06-23 22:56:06 -0400

    fixedContent

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <>
       <div className="flex h-[90vh] w-screen flex-col items-center justify-center md:flex-row">
-        <div className=" flex h-[50%] w-full flex-col items-center justify-start gap-y-0 p-5 md:h-[70%] md:justify-center md:gap-x-4 md:gap-y-6 md:p-10">
+        <div className=" mt-[15%] flex h-[50%] w-full flex-col items-center justify-start gap-y-0 p-5 md:mt-[0] md:h-[70%] md:justify-center md:gap-x-4 md:gap-y-6 md:p-10">
           <h1 className="text-2xl font-bold md:text-4xl">Contact</h1>
           <p>
             FIN STUDIO ARCHITECT LIMITED
@@ -22,7 +22,7 @@ const Contact = () => {
         <div className="flex h-[50%] w-full items-center justify-center overflow-hidden bg-cover md:h-[70%]">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15490.467505245035!2d100.438514310702!3d13.921833811271211!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30e2857afab65b13%3A0xd9efdb3a1c55c3b9!2z4Liq4Lix4Lih4Lih4Liy4LiB4Lij!5e0!3m2!1sen!2sus!4v1687573549858!5m2!1sen!2sus"
-            className="mr-8 h-full w-full"
+            className="mx-2 mb-[5%] h-full w-full md:mx-8 md:mb-[0]"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
