b3b82afd2e430c581c40862a88f8df0c6e55bd65
Thanat <bztzn512@gmail.com>
2023-08-07 12:35:42 -0400

    fixedFloder

---
+++ src/app/works/render/exterior/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Exterior = () => {
+  return <div>Exterior</div>;
+};
+
+export default Exterior;
---
+++ src/app/works/render/interior/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Interior = () => {
+  return <div>Interior</div>;
+};
+
+export default Interior;
