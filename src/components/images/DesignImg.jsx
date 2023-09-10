31dac6d58a787065a15aa358d0ce36329c10189c
Thanat <bztzn512@gmail.com>
2023-09-09 20:45:08 -0400

    .

--- src/components/images/DesignImg.jsx
+++ src/components/images/DesignImg.jsx
@@ -1,7 +1,3 @@
-import ImgOne from "../../../public/work/architecture design/01อาคารปฏิบัติธรรม เฉลิมพระเกียรติ วัดวังก์พง/1.jpg";
-import ImgTwo from "../../../public/work/architecture design/01อาคารปฏิบัติธรรม เฉลิมพระเกียรติ วัดวังก์พง/2.jpg";
-import ImgThree from "../../../public/work/architecture design/01อาคารปฏิบัติธรรม เฉลิมพระเกียรติ วัดวังก์พง/3.jpg";
-
 export const designImg = [
   {
     image: ImgOne,
